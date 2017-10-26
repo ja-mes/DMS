b4519afd270e9c308c82b6ca9219c555d5137930
James <jamezbrown100@gmail.com>
2017-10-26 16:30:52 -0500

    Setup more pages

--- app/controllers/vehicle_controller.rb
+++ app/controllers/vehicle_controller.rb
@@ -1,4 +1,22 @@
 class VehicleController < ApplicationController
     def index
+        @vehicles = Vehicle.all
+    end
+
+    def new
+        @vehicle  = Vehicle.new
+    end
+
+    def create
+        @vehicle = Vehicle.new(vehicle_params)
+        if @vehicle.save
+            flash[:success] = "Vehicle successfully created"
+        end
+    end
+
+
+    private
+    def vehicle_params
+        params.require(:vehicle).permit(:make, :model)
     end
 end
