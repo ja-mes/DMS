dc123b21343b068834bee0fde75c02a726719391
James <jamezbrown100@gmail.com>
2017-11-02 05:42:16 -0500

    Create customers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102102038) do
+ActiveRecord::Schema.define(version: 20171102104158) do
+
+  create_table "customers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "vehicles", force: :cascade do |t|
     t.string "year"
